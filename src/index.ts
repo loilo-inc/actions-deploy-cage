cfd5291db7b2bf01803051290fb6e56b1da11149
keroxp <kerokerokerop@gmail.com>
2019-11-15 18:09:13 +0900

    fix: assert region and deploy-context

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,24 @@ function boolify(s: string): boolean {
   return s !== "" && !s.match(/^(false|0|undefined|null)$/);
 }
 
+function assertInput(name: string): string {
+  const v = core.getInput(name);
+  if (!v) {
+    throw new Error(`${name} is required`);
+  }
+  return v;
+}
+
 async function main() {
+  const version = core.getInput("cage-version");
+  const deployContext = assertInput("deploy-context");
+  const region = assertInput("region");
+  const createDeployment = boolify(core.getInput("create-deployment"));
+  const environment = core.getInput("environment");
+  const token = core.getInput("github-token");
+  const ref = core.getInput("github-ref");
+  const repository = core.getInput("github-repository");
   try {
-    const version = core.getInput("cage-version");
-    const region = core.getInput("region");
-    const deployContext = core.getInput("deploy-context");
-    const createDeployment = boolify(core.getInput("create-deployment"));
-    const environment = core.getInput("environment");
-    const token = core.getInput("github-token");
-    const ref = core.getInput("github-ref");
-    const repository = core.getInput("github-repository");
     if (!(await io.which("cage", false))) {
       await downloadCage({ version });
     }
