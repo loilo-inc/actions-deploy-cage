f1809653a10192780e256e8d9771f09c792c2cad
Yusuke Sakurai <kerokerokerop@gmail.com>
2020-12-02 20:28:15 +0900

    v2

--- src/deploy.test.ts
+++ src/deploy.test.ts
@@ -1,8 +1,23 @@
-import { parseRef } from "./deploy";
-
+import {getLatestVersion, parseRef} from "./deploy";
+import * as nock from "nock"
 describe("deploy", () => {
   test("parseRef", () => {
     expect(parseRef("refs/heads/master")).toBe("master");
     expect(parseRef("refs/tags/v0.1.0")).toBe("v0.1.0");
   });
+  describe("getLatestVersions", () => {
+    test("basic", async () => {
+      nock("https://api.github.com")
+        .get("/repos/loilo-inc/canarycage/releases")
+        .reply(200, [{tag_name: "1.0.0"}, {tag_name: "2.0.0"}, {tag_name: "0-0"}])
+      const latest = await getLatestVersion()
+      expect(latest).toBe("2.0.0")
+    })
+    test("should throw if status is not 200", async () => {
+      nock("https://api.github.com")
+        .get("/repos/loilo-inc/canarycage/releases")
+        .reply(500, {})
+      await expect(getLatestVersion()).rejects.toThrowError(Error)
+    })
+  })
 });
