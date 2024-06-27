75855b835b193104c41e233a7993db74e9e04fdb
Yusuke Sakurai <kerokerokerop@gmail.com>
2024-06-27 15:57:43 +0900

    feat: add cage-options (#32)

---
+++ src/__tests__/args.test.ts
@@ -0,0 +1,13 @@
+import { parseStringToArgs } from "../args";
+
+describe("args", () => {
+  test.each([
+    ["--opt1 10 --opt2", ["--opt1", "10", "--opt2"]],
+    [
+      "--opt1 10 --opt2 --opt3 \"--foo bar\" --opt4 'o'",
+      ["--opt1", "10", "--opt2", "--opt3", "--foo bar", "--opt4", "o"],
+    ],
+  ])("basic", (v, exp) => {
+    expect(parseStringToArgs(v)).toEqual(exp);
+  });
+});
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,56 @@
+import * as core from "@actions/core";
+import * as deploy from "../deploy";
+import { main } from "..";
+
+describe("index", () => {
+  const input = (key: string, value: string) => {
+    jest.spyOn(core, "getInput").mockImplementationOnce((name: string) => {
+      expect(name).toBe(key);
+      return value;
+    });
+  };
+  test("basic", async () => {
+    input("deploy-context", ".");
+    input("region", "us-west-2");
+    input("create-deployment", "true");
+    input("environment", "production");
+    input("canary-task-idle-duration", "10");
+    input("update-service", "true");
+    input("cage-options", "--foo 'bar' --baz");
+    input("github-token", "token");
+    input("github-ref", "refs/heads/main");
+    input("github-repository", "owner/repo");
+    const deploySpy = jest.spyOn(deploy, "deploy").mockResolvedValueOnce();
+    await main();
+    expect(deploySpy).toHaveBeenCalledWith({
+      deployment: {
+        environment: "production",
+        ref: "main",
+        owner: "owner",
+        repo: "repo",
+        token: "token",
+      },
+      args: [
+        "--region",
+        "us-west-2",
+        "--canaryTaskIdleDuration",
+        "10",
+        "--updateService",
+        "--foo",
+        "bar",
+        "--baz",
+        ".",
+      ],
+    });
+  });
+  test("minimal", async () => {
+    input("deploy-context", ".");
+    input("region", "us-west-2");
+    const deploySpy = jest.spyOn(deploy, "deploy").mockResolvedValueOnce();
+    await main();
+    expect(deploySpy).toHaveBeenCalledWith({
+      deployment: undefined,
+      args: ["--region", "us-west-2", "."],
+    });
+  });
+});
